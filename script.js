9ae4920155c5baba05c365d8740826d046eb33d1
C.V.K. Bhanu <47163716+bhanucvk@users.noreply.github.com>
2025-04-13 07:06:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('feedbackForm').addEventListener('submit', function(e) {
   e.preventDefault();
   const name = document.getElementById('name').value.trim();
-  const feedback = document.getElementById('feedback').value.trim();
+  const feedback = document.getElementById('Rate the website on a scale of "10"').value.trim();
 
   if (name && feedback) {
     document.getElementById('message').innerText = `Thank you for your feedback, ${name}!`;
